ede397abe6bc5f700a0a1bcd094d38c0dd517fb7
Rogério-Mint <rogerio.barbosa.pf@gmail.com>
2023-04-09 12:39:55 -0300

    Fazendo o migrate no BD

--- usuarios.py
+++ usuarios.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from models import Usuario
 
 
 bp_usuarios = Blueprint('usuarios', __name__, template_folder='templates')
@@ -7,4 +8,13 @@ bp_usuarios = Blueprint('usuarios', __name__, template_folder='templates')
 
 @bp_usuarios.route('/create', methods=['GET', 'POST'])
 def create():
-    return render_template('usuarios_create.html')
+    if request.method=='GET':
+        return render_template('usuarios_create.html')
+    
+    if request.method=='POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        csenha = request.form.get('csenha')
+
+        #return 'Dados recebidos.'
